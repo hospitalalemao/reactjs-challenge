0c203459e496e008c788b4a76c7437e08480261a
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-01 15:32:53 -0300

    feat: Create global style component

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+
+  body {
+    font-size: 16px;
+    background: ${({ theme }) => theme.color.background};
+    font-display: swap;
+    font-weight: 400;
+    transition: all 0.5s ease-out;
+  }
+
+  :root {
+    --primary: ${({ theme }) => theme.color.primary};
+    --secondary: ${({ theme }) => theme.color.secondary};
+    --background: ${({ theme }) => theme.color.background};
+  }
+`;
