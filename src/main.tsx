64a22f2af98e020b0fb1573c12d8b040a4a2aa52
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-01 13:47:29 -0300

    feat: Add react-router-dom with BrazilPage and HomePage

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BrazilPage from './pages/BrazilPage';
+import HomePage from './pages/HomePage';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/brazil" element={<BrazilPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/BrazilPage/index.tsx
@@ -0,0 +1,3 @@
+export default function BrazilPage() {
+  return <h1>Brazil page</h1>;
+}
