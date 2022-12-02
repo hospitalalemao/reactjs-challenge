771dc71d43a571a02cafb7eab5ecf2831b90df92
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-02 12:43:40 -0300

    feat: Adds method that calculates the total value of deaths, recoveries and contaminations in the country

--- src/features/provinces.ts
+++ src/features/provinces.ts
@@ -4,6 +4,8 @@ export const provinces = createSlice({
   name: 'provinces',
   initialState: {
     value: [],
+    total: {},
+    selected: {},
   },
   reducers: {
     formatProvinces: (state, action) => {
@@ -11,17 +13,40 @@ export const provinces = createSlice({
       const provincesObj = action.payload;
       const provincesObjLength = Object.keys(provincesObj).length;
 
+      let totalValues = {
+        confirmed: 0,
+        confirmed_diff: 0,
+        deaths: 0,
+        deaths_diff: 0,
+        recovered: 0,
+        recovered_diff: 0,
+        fatality_rate: 0,
+      };
+
       for (let i = 0; i <= provincesObjLength; i += 1) {
         if (provincesObj[i]) {
+          totalValues = {
+            confirmed: totalValues.confirmed + provincesObj[i].confirmed,
+            confirmed_diff:
+              totalValues.confirmed_diff + provincesObj[i].confirmed_diff,
+            deaths: totalValues.deaths + provincesObj[i].deaths,
+            deaths_diff: totalValues.deaths_diff + provincesObj[i].deaths_diff,
+            recovered: totalValues.recovered + provincesObj[i].active,
+            recovered_diff:
+              totalValues.recovered_diff + provincesObj[i].active_diff,
+            fatality_rate:
+              totalValues.fatality_rate + provincesObj[i].fatality_rate,
+          };
+
           provincesList.push({
             index: i,
             name: provincesObj[i].region.province,
             date: provincesObj[i].date,
-            confirmed: provincesObj[i].confirmed,
-            deaths: provincesObj[i].deaths,
-            confirmed_diff: provincesObj[i].confirmed_diff,
-            deaths_diff: provincesObj[i].deaths_diff,
             last_update: provincesObj[i].last_update,
+            confirmed: provincesObj[i].confirmed,
+            confirmed_diff: provincesObj[i].confirmed_diff,
+            deaths: provincesObj[i].deaths,
+            deaths_diff: provincesObj[i].deaths_diff,
             recovered: provincesObj[i].active,
             recovered_diff: provincesObj[i].active_diff,
             fatality_rate: provincesObj[i].fatality_rate,
@@ -29,7 +54,9 @@ export const provinces = createSlice({
         }
       }
 
+      state.total = totalValues;
       state.value = provincesList;
+      state.selected = provincesList[0];
     },
     sortProvinces: (state, action) => {
       const { field, orderText } = action.payload;
@@ -37,11 +64,11 @@ export const provinces = createSlice({
       if (field) {
         const order = orderText === 'asc' ? 1 : -1;
 
-        state.value.sort((stateA: any, stateB: any) => {
-          if (stateA[field] > stateB[field]) {
+        state.value.sort((provinceA: any, provinceB: any) => {
+          if (provinceA[field] > provinceB[field]) {
             return 1 * order;
           }
-          if (stateA[field] < stateB[field]) {
+          if (provinceA[field] < provinceB[field]) {
             return -1 * order;
           }
           return 0;
