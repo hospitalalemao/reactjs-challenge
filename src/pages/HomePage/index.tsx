31d15e87af341447ba085b5b28ac926e38fafe15
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-03 20:35:50 -0300

    feat: Adds date filter to Home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,20 +1,31 @@
 import { useQuery } from '@tanstack/react-query';
+import { useSelector } from 'react-redux';
+import DatePickerComponent from '../../components/DatePickerComponent';
 import api from '../../services/covid_statics_api';
 
 function HomePage() {
+  const { dateFilter } = useSelector((state) => state.dateFilter);
   const getData = async (): Promise<object> => {
-    const response = await api.get('reports/total');
+    const params = dateFilter
+      ? {
+          params: { date: dateFilter },
+        }
+      : {};
+
+    const response = await api.get('reports/total', params);
+
     return { ...response.data.data };
   };
 
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['reports/total'],
+    queryKey: ['reports/total', dateFilter],
     queryFn: getData,
   });
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
+
   if (isError) {
     return <h1>Error ao requisitar os dados. Por favor, tente outra vez.</h1>;
   }
@@ -22,6 +33,7 @@ function HomePage() {
   return (
     <div>
       <h1>World Report Page</h1>
+      <DatePickerComponent />
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
