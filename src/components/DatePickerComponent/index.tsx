896cbd9b2c34c2ca1db093fabd707caff630a57a
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-12-04 11:50:30 -0300

    style: Adds style to date picker component

--- src/components/DatePickerComponent/index.tsx
+++ src/components/DatePickerComponent/index.tsx
@@ -2,13 +2,17 @@ import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import pt from 'date-fns/locale/pt';
 import { useDispatch, useSelector } from 'react-redux';
-import { subDays } from 'date-fns';
+import { format, subDays } from 'date-fns';
+import { useState } from 'react';
+import { FaCalendarAlt } from 'react-icons/fa';
 import { setDateFilter } from '../../features/dateFilter';
+import * as S from './styles';
 
 registerLocale('pt', pt);
 
 export default function DatePickerComponent() {
   const { date } = useSelector((state) => state.dateFilter);
+  const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
   const YEAR = 2020;
   const MONTH = 1;
@@ -16,16 +20,34 @@ export default function DatePickerComponent() {
   const maxDate = subDays(new Date(), 1);
   const minDate = new Date(YEAR, MONTH, DAY);
 
+  const handleChange = (selectedDate: string) => {
+    dispatch(setDateFilter(selectedDate));
+    setIsOpen(!isOpen);
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <DatePicker
-      locale="pt"
-      minDate={minDate}
-      maxDate={maxDate}
-      selected={new Date(date)}
-      showDisabledMonthNavigation
-      onChange={(selectedDate) =>
-        dispatch(setDateFilter((selectedDate || '').toString()))
-      }
-    />
+    <S.DatePickerContainer>
+      <S.DatePickerButton onClick={handleClick}>
+        <h3>{format(new Date(date), 'dd/MM/yyyy')}</h3>
+        <FaCalendarAlt />
+      </S.DatePickerButton>
+      {isOpen && (
+        <DatePicker
+          inline
+          locale="pt"
+          minDate={minDate}
+          maxDate={maxDate}
+          selected={new Date(date)}
+          showDisabledMonthNavigation
+          onChange={(selectedDate) =>
+            handleChange((selectedDate || '').toString())
+          }
+        />
+      )}
+    </S.DatePickerContainer>
   );
 }
---
+++ src/components/DatePickerComponent/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { ButtonHeader } from '../Table/styles';
+
+export const DatePickerContainer = styled.div`
+  width: 200px;
+`;
+
+export const DatePickerButton = styled(ButtonHeader)`
+  color: var(--title-color);
+`;
